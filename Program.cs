29483b54ffd015a92c083a934d19978188ecccd4
Vedant <vedantnp200@gmail.com>
2023-05-22 15:38:35 +0530

    Check employee attendance program is added.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Employee_Wage_Computation_Program;
 
 namespace EmployeeWageComputationProgram
 {
@@ -6,7 +7,25 @@ namespace EmployeeWageComputationProgram
     {
         public static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Enter your Choice: ");
+                Console.WriteLine("1. Check employee is present or not.");
 
+                Console.Write("=>");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Attendance.EmployeeAttendance();
+                        break;
+                    default: 
+                        Console.WriteLine("Enter correct choice.");
+                        break;
+                }
+                Console.WriteLine("---------------------------------------");
+            }
         }
     }
 }
