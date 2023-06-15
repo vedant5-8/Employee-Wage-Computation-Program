01594c92892c03b36b924ed4ae21baa603c839af
Vedant <vedantnp200@gmail.com>
2023-06-15 12:34:32 +0530

    [Vedant]-Check Employee is present or absent

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 
-namespace EmployeeWageComputationProgram
+namespace Employee_Wage_Computation_Program
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            // UC1- Check employee is present or not
+
+            int Is_Full_Time = 1;
+
+            Random random = new Random();
+
+            int EmpCheck = random.Next(0, 2);
+
+            if (EmpCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
+
         }
     }
 }
