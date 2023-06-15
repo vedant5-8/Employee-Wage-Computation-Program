35e411388d0d2fbed2b76cfd803481c9a0fe5ad0
Vedant <vedantnp200@gmail.com>
2023-06-15 15:59:56 +0530

    [Vedant]-Calculate total employee wage and save employee wage by company.

---
+++ EmpWageBuilder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation_Program
+{
+    // UC9 - Calculate total employee wage and save employee wage by company.
+    public class EmpWageBuilder
+    {
+        public const int Is_Part_Time = 1;
+        public const int Is_Full_Time = 2;
+
+        private string Company;
+        private int EmpRatePerHour;
+        private int NumOfWorkDays;
+        private int MaxHoursPerMonth;
+        private int TotalEmpWage;
+
+        public EmpWageBuilder(string Company, int EmpRatePerHour, int NumOfWorkDays, int MaxHoursPerMonth)
+        {
+            this.Company = Company;
+            this.EmpRatePerHour = EmpRatePerHour;
+            this.NumOfWorkDays = NumOfWorkDays;
+            this.MaxHoursPerMonth = MaxHoursPerMonth;
+        }
+
+        public void ComputeEmpWage()
+        {
+            int EmpHours;
+            int TotalEmpHours = 0;
+            int TotalWorkingDays = 0;
+
+            while (TotalEmpHours <= this.MaxHoursPerMonth && TotalWorkingDays < this.NumOfWorkDays)
+            {
+                TotalWorkingDays++;
+
+                Random random = new Random();
+
+                int EmpCheck = random.Next(0, 3);
+
+                switch (EmpCheck)
+                {
+                    case Is_Part_Time:
+                        EmpHours = 4;
+                        break;
+                    case Is_Full_Time:
+                        EmpHours = 8;
+                        break;
+                    default:
+                        EmpHours = 0;
+                        break;
+                }
+
+                TotalEmpHours += EmpHours;
+                Console.WriteLine("Days: {0} | Employee Hours: {1}", TotalWorkingDays, EmpHours);
+
+            }
+
+            TotalEmpWage = TotalEmpHours * this.EmpRatePerHour;
+
+        }
+
+        public string toString()
+        {
+            return "\nTotal employee wage for company " + this.Company + " is " + this.TotalEmpWage + "\n";
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,50 +4,17 @@ namespace Employee_Wage_Computation_Program
 {
     class Program
     {
-        public const int Is_Part_Time = 1;
-        public const int Is_Full_Time = 2;
-
-        // UC8 - Calculate employee wage for multiple companies.
-        public static int ComputeEmpWage(string Company, int EmpRatePerHour, int NumOfWorkDays, int MaxHoursPerMonth)
-        {
-            int EmpHours;
-            int TotalEmpHours = 0;
-            int TotalWorkingDays = 0;
-
-            while (TotalEmpHours <= MaxHoursPerMonth && TotalWorkingDays < NumOfWorkDays)
-            {
-                TotalWorkingDays++;
-
-                Random random = new Random();
-
-                int EmpCheck = random.Next(0, 3);
-
-                switch (EmpCheck)
-                {
-                    case Is_Part_Time:
-                        EmpHours = 4;
-                        break;
-                    case Is_Full_Time:
-                        EmpHours = 8;
-                        break;
-                    default:
-                        EmpHours = 0;
-                        break;
-                }
-
-                TotalEmpHours += EmpHours;
-                Console.WriteLine("Days: {0} | Employee Hours: {1}", TotalWorkingDays, EmpHours);
-
-            }
-
-            int TotalEmpWage = TotalEmpHours * EmpRatePerHour;
-            Console.WriteLine("\nTotal employee wage for company {0} is {1}\n",Company, TotalEmpWage);
-            return TotalEmpWage;
-        }
+        
         static void Main(string[] args)
         {
-            ComputeEmpWage("IBM", 20, 5, 25);
-            ComputeEmpWage("Jio", 20, 4, 20);
+            EmpWageBuilder IBM = new EmpWageBuilder("IBM", 20, 2, 10);
+            EmpWageBuilder Jio = new EmpWageBuilder("Jio", 10, 4, 20);
+
+            IBM.ComputeEmpWage();
+            Console.WriteLine(IBM.toString());
+
+            Jio.ComputeEmpWage();
+            Console.WriteLine(Jio.toString());
         }
     }
 }
