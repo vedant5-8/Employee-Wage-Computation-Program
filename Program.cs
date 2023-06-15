5e621067df677e9a26dd13a081c5e958a0cbc3f1
Vedant <vedantnp200@gmail.com>
2023-06-15 14:30:00 +0530

    [Vedant]-Calculate monthly employee wage.

--- Program.cs
+++ Program.cs
@@ -7,32 +7,42 @@ namespace Employee_Wage_Computation_Program
         public const int Is_Part_Time = 1;
         public const int Is_Full_Time = 2;
         public const int EmpRatePerHour = 20;
+        public const int NumOfWorkDaysPerMonth = 20;
         static void Main(string[] args)
         {
-            // UC4 - Calculate daily employee wage with switch case statement.
+            // UC5 - Calculate monthly employee wage.
 
             int EmpHours;
             int EmpWage;
+            int TotalEmpWage = 0;
 
-            Random random = new Random();
-
-            int EmpCheck = random.Next(0, 3);
-
-            switch (EmpCheck)
+            for (int day = 1; day <= NumOfWorkDaysPerMonth; day++)
             {
-                case Is_Part_Time:
-                    EmpHours = 4;
-                    break;
-                case Is_Full_Time:
-                    EmpHours = 8;
-                    break;
-                default:
-                    EmpHours = 0;
-                    break;
+                Random random = new Random();
+
+                int EmpCheck = random.Next(0, 3);
+
+                switch (EmpCheck)
+                {
+                    case Is_Part_Time:
+                        EmpHours = 4;
+                        break;
+                    case Is_Full_Time:
+                        EmpHours = 8;
+                        break;
+                    default:
+                        EmpHours = 0;
+                        break;
+                }
+
+                EmpWage = EmpHours * EmpRatePerHour;
+                TotalEmpWage += EmpWage;
+                Console.WriteLine("Daily Employee Wage is {0}", EmpWage);
+
             }
 
-            EmpWage = EmpHours * EmpRatePerHour;
-            Console.WriteLine("Daily Employee Wage is {0}", EmpWage);
+            Console.WriteLine("\nTotal employee wage is {0}", TotalEmpWage);
+
         }
     }
 }
