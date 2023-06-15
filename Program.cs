5080322d4197cf9d8f3e6630068094bbec867a61
Vedant <vedantnp200@gmail.com>
2023-06-15 15:24:40 +0530

    [Vedant]-Calculate employee wage using class methods

--- Program.cs
+++ Program.cs
@@ -9,12 +9,11 @@ namespace Employee_Wage_Computation_Program
         public const int EmpRatePerHour = 20;
         public const int NumOfWorkDaysPerMonth = 20;
         public const int MaxHoursInMonth = 100;
-        static void Main(string[] args)
-        {
-            // UC6 - Calculate Wages till number of working days or total working hours per months is reached
 
+        // UC7 - Calculate employee wage using class methods
+        public static int ComputeEmpWage()
+        {
             int EmpHours;
-            int EmpWage;
             int TotalEmpHours = 0;
             int TotalWorkingDays = 0;
 
@@ -40,13 +39,17 @@ namespace Employee_Wage_Computation_Program
                 }
 
                 TotalEmpHours += EmpHours;
-                Console.WriteLine("Days: {0} | Employee Hours: {1}",TotalWorkingDays, EmpHours);
+                Console.WriteLine("Days: {0} | Employee Hours: {1}", TotalWorkingDays, EmpHours);
 
             }
 
             int TotalEmpWage = TotalEmpHours * EmpRatePerHour;
             Console.WriteLine("\nTotal employee wage is {0}", TotalEmpWage);
-
+            return TotalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            ComputeEmpWage();
         }
     }
 }
